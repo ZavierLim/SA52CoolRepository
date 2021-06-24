041620abf5c4e365373b27876277b19be1a1b526
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-06-24 15:03:29 +0800

    first push without properties

---
+++ ToplayWithGit/src/main/java/sg/edu/iss/harrison/ToplayWithGitApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.harrison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToplayWithGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToplayWithGitApplication.class, args);
+	}
+
+}
